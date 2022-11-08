e044619fd145b110d033c2e5552db4b25b7a23bc
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2022-11-08 00:17:55 -0300

    fetch animais refatorado

--- js/modules/animais-fetch.js
+++ js/modules/animais-fetch.js
@@ -1,6 +1,6 @@
 import AnimaNumeros from './anima-numeros.js';
 
-export default function initAnimaisFetch() {
+export default function fetchAnimais(url, target) {
   function createDiv(animal) {
     const div = document.createElement('div');
     div.classList.add('numero-animal');
@@ -8,21 +8,22 @@ export default function initAnimaisFetch() {
     return div;
   }
 
-  async function animaisFetch(url) {
+  async function criarAnimais() {
     try {
       const response = await fetch(url);
       const responseJson = await response.json();
-      const gridAnimal = document.querySelector('.grid-numeros');
+      const gridAnimal = document.querySelector(target);
 
       responseJson.forEach((animal) => {
         const div = createDiv(animal);
         gridAnimal.appendChild(div);
       });
+
       const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
       animaNumeros.init();
     } catch (erro) {
       console.log(erro);
     }
   }
-  animaisFetch('./animaisapi.json');
+  return criarAnimais(url);
 }
